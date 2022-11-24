f89f9ad0f4fe648cacc07813f46dad5537e87895
Teshi <dj.mobile.h@gmail.com>
2022-11-24 23:57:18 +0100

    ping upgrade

--- commands/ping.js
+++ commands/ping.js
@@ -1,10 +1,16 @@
 const { SlashCommandBuilder } = require('discord.js');
-
+const dc = require('../libs/dc');
+const ms = require('ms');
 module.exports = {
   data: new SlashCommandBuilder()
     .setName('ping')
     .setDescription('Replies with Pong!'),
   async execute(interaction) {
-    await interaction.reply('Pong!');
+    const rec = new Date() - interaction.createdTimestamp;
+    const text = "`" + rec + "ms` speed\n\n`" + interaction.client.ws.ping + "ms` ping\n\n`" + ms(interaction.client.uptime) + "` uptime"
+    const embed = dc.sEmbed('PONG      🏓', text, 'Tesh läuft auf deutschen Servern!', '0xaaeeff');
+    await interaction.reply({
+      embeds: [embed]
+    });
   },
 };
