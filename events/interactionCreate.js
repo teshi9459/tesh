8e5247b8709a0ef0ab65b51fe11ff7ae44a9ef25
teshi9459 <dj.mobile.h@gmail.com>
2024-09-12 20:56:08 +0000

    new eventhandling

--- events/interactionCreate.js
+++ events/interactionCreate.js
@@ -1,17 +1,22 @@
-const fs = require('node:fs/promises');
-const path = require('node:path');
-const ticketManager = require('../exports/message/ticket');
-const { Client, Collection, GatewayIntentBits } = require('discord.js');
-const db = require('../libs/db');
+const fs = require("node:fs/promises");
+const path = require("node:path");
+const ticketManager = require("../exports/message/ticket");
+const { Client, Collection, GatewayIntentBits } = require("discord.js");
+const db = require("../libs/db");
 const client = new Client({ intents: [GatewayIntentBits.Guilds] });
 module.exports = {
-  name: 'interactionCreate',
+  name: "interactionCreate",
   once: false,
   async execute(interaction) {
-    
-    switch (interaction.commandType) {
-      case 1: //slash commands
-        const plugin = interaction.client.plugins.get(interaction.commandName);
+    console.log(interaction.toJSON());
+    console.log(interaction.type);
+    let plugin = null;
+    switch (interaction.type) {
+      case 1: //slash commands ??
+        break;
+
+      case 2: //UI-based command User (slashs)
+         plugin = interaction.client.plugins.get(interaction.commandName);
         if (!plugin) return;
         if (!plugin.enabled) return;
         if (!plugin.slashCommands) return;
@@ -19,29 +24,28 @@ module.exports = {
           await plugin.executeSlashCommand(interaction);
         } catch (error) {
           console.error(error);
-          db.logError('bot', 'itc create', error);
+          //db.logError('bot', 'itc create', error);
         }
         break;
 
-      case 2://UI-based command User
-        
-      console.log(interaction.type);
-        break;    
-
-      case 3:   //message
-
-      console.log(interaction.type);
+      case 3: //message (buttons)
+       plugin = interaction.client.plugins.get(interaction.customId.split('.')[0]);
+        if (!plugin) return;
+        if (!plugin.enabled) return;
+        if (!plugin.buttons) return;
+        try {
+          await plugin.executeButton(interaction);
+        } catch (error) {
+          console.error(error);
+          //db.logError('bot', 'itc create', error);
+        }
         break;
 
       case 4: //UI-based command PrimaryEntryPoint
-
-      console.log(interaction.type);
         break;
-    
+
       default:
         break;
     }
-
-
   },
 };
--- events/messageCreate.js
+++ events/messageCreate.js
@@ -6,29 +6,8 @@ module.exports = {
     once: false,
     async execute(message) {
         if (message.author.bot) return;
-        const commandFiles = fs.readdirSync('./exports/message/').filter(file => file.endsWith('.js'));
-        for (const file of commandFiles) {
-            const modul = require(`../exports/message/${file}`);
-
-            switch (modul.name) {
-                case 'words':
-                    if (!await db.checkWordsToggle(message.guildId)) continue;
-                    break;
-                case 'ticket':
-                    break;
-                default:
-                    continue;
-                    break;
-            }
-
-            try {
-                if (!await db.checkUser(message.author.id) && await db.checkGuild(message.guildId)) db.insertUser(message.author.id);
-                modul.execute(message);
-            } catch (error) {
-                console.error(error);
-                db.logError('bot', 'msg create', error);
-                message.reply('Ein Fehler ist aufgetreten qwq\n*kontaktiere den Developer*');
-            }
-        }
+        message.client.plugins.forEach(plugin => {
+            if (plugin.enabled && plugin.messages) plugin.executeMessage(message);
+        });
     },
 };
