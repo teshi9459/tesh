6c485c820855d762a39323f6a719a85275cc80ed
teshi9459 <dj.mobile.h@gmail.com>
2024-09-12 20:55:28 +0000

    basic ticket struckture

--- plugins/ticket.js
+++ plugins/ticket.js
@@ -1,34 +1,112 @@
-const { SlashCommandBuilder } = require('discord.js');
+const { SlashCommandBuilder, ButtonStyle, ActionRowBuilder } = require('discord.js');
 const dc = require('../libs/dc');
-const ms = require('ms');
+const e = require('cors');
 module.exports = {
     //info
-    name: 'ping',
+    name: 'ticket',
     enabled: true,
     //interctions
     slashCommands: true,
     contectMenues: false,
-    buttons: false,
+    buttons: true,
     selectMenues: false,
     modal: false,
-    messages: false,
+    messages: true,
     //categorys
     database: false,
     logging: false,
     moderation: false,
-    embedresponse: true,
+    ephemeralresponse: true,
     setup: false,
     //SlashCommands Builder
     data: new SlashCommandBuilder()
-    .setName('ping')
-    .setDescription('Replies with Pong!'),
-
+    .setName('ticket')
+    .setDescription('bearbeite das Ticketsystem')
+    .addSubcommand(subcommand => subcommand
+            .setName('panel')
+            .setDescription('erstelle ein neues Ticketpanel'))
+    .addSubcommand(subcommand => subcommand
+            .setName('close')
+            .setDescription('schließt das Ticket im aktuellen Channel'))
+    .addSubcommand(subcommand => subcommand
+            .setName('status')
+            .setDescription('ändere den Status des Tickets')
+            .addStringOption(option => option
+                .setName('type')
+                .setDescription('typ der aktuellen Situation')
+                .setRequired(true)
+                .addChoices(
+                        { name: 'wird bearbeitet', value: 'work' },
+                        { name: 'warten auf Antwort', value: 'wait' },
+                        { name: 'Hilfe benötigt', value: 'help' },
+                        { name: 'Sonderfall', value: 'spec' },
+                        { name: 'Neu', value: 'newt' },
+                    )))
+    
+    ,
   async executeSlashCommand(interaction) {
-    //maybe add db ping and webiste ping
-    const text = "`" + interaction.client.ws.ping + "ms` ping\n\n`" + ms(interaction.client.uptime) + "` uptime\n";
-    const embed = dc.sEmbed('PONG      🏓', text, 'Tesh\'s direkter Ping aus Teshi\'s Schlafzimmer!!', '0xaaeeff');
-    await interaction.reply({
-      embeds: [embed]
-    });
+    //permissons check
+    if(!interaction.isChatInputCommand()) return;
+    let button = null;
+    let answer = 'keine passende Auswahl'
+    switch (interaction.options.getSubcommand()) {
+        case 'panel':
+            answer = '⬇️⬇️⬇️';
+            button = dc.createButton('https://localhost', 'neues Panel', ButtonStyle.Link);
+            break;
+    
+        case 'close':
+            //send close befehel
+            answer = 'Ticket wird geschlossen, dies kann ein Moment dauern';
+            button = dc.createButton(this.name + '.close.' + interaction.channelId, 'schließen', ButtonStyle.Danger);
+            break;
+    
+        case 'status':
+            //ticketchannel check
+            let statuslong;
+            const name = interaction.channel.name.slice(1);
+            switch (interaction.options.getString('type')) {
+                case 'work':
+                    statuslong = 'wird bearbeitet';
+                    interaction.channel.edit({ name: '🔧' + name });
+                    break;
+                case 'wait':
+                    statuslong = 'warten auf User';
+                    interaction.channel.edit({ name: '🕒' + name });
+                    break;
+                case 'help':
+                    statuslong = 'Hilfe benötigt';
+                    interaction.channel.edit({ name: '🚸' + name });
+                    break;
+                case 'spec':
+                    statuslong = 'Sonderfall';
+                    interaction.channel.edit({ name: '⭕' + name });
+                    break;
+                case 'newt':
+                    statuslong = 'neues Ticket';
+                    interaction.channel.edit({ name: '🆕' + name });
+                    break;
+                default:
+                    break;
+            }
+            answer = 'Ticket Status wurde angepasst zu `' + statuslong + '` !'
+            break;
+        default:
+            break;
+    }
+    let row = undefined;
+    if(button !== null){
+    row = new ActionRowBuilder()
+			.addComponents(button);
+    row = [row];
+    }
+    await interaction.reply({ embeds: [dc.sEmbed('Ticket', answer , 'Ticket System', '0xaaeeff')], components: row, ephemeral: true });
   },
+  async executeMessage(message) {
+    console.log(message.content);
+  },
+  async executeButton(button) {
+    button.meta = button.customId.split('.');
+    console.log(button.customId);
+  }
 };
